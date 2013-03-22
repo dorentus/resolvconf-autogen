bc28f8c5ba5ed7e1abe873e43e132491112d53f9
Zhang Yi <zhangyi.cn@gmail.com>
2013-03-22 19:35:35 +0800

    done

---
+++ gw-monitor/ApplicationDelegate.h
@@ -0,0 +1,16 @@
+//
+//  ApplicationDelegate.h
+//  gw-monitor
+//
+//  Created by Zhang Yi on 13-3-22.
+//  Copyright (c) 2013年 Zhang Yi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+
+@interface ApplicationDelegate : NSObject<NSApplicationDelegate>
+
+- (id)initWithFileURL:(NSURL *)url;
+
+@end
---
+++ gw-monitor/main.m
@@ -0,0 +1,39 @@
+//
+//  main.m
+//  gw-monitor
+//
+//  Created by Zhang Yi on 13-3-22.
+//  Copyright (c) 2013年 Zhang Yi. All rights reserved.
+//
+
+#import <CoreFoundation/CoreFoundation.h>
+#import <AppKit/AppKit.h>
+#import "ApplicationDelegate.h"
+#include <libgen.h>
+
+int main(int argc, const char * argv[])
+{
+    if (argc != 2) {
+        printf("Monitor network change and write current gateway to file (resolv.conf format)\n\nUsage: %s <output-file>", basename((char *)argv[0]));
+        return 0;
+    }
+
+    @autoreleasepool {
+        NSString *filepath = [NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding];
+        NSURL *fileURL = [NSURL fileURLWithPath:filepath];
+
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        if (![fileManager isWritableFileAtPath:filepath]) {
+            printf("Cannot access file: 『%s』", [fileURL.absoluteString UTF8String]);
+            return 0;
+        }
+
+        ApplicationDelegate *delegate = [[ApplicationDelegate alloc] initWithFileURL:fileURL];
+        NSApplication *app = [NSApplication sharedApplication];
+        [app setDelegate:delegate];
+        [app run];
+    }
+
+    return 0;
+}
+
